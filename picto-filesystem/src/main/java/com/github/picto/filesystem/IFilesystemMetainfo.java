0c65bf9ecd37f51b1b9beecda7d660d194a6e673
Pierre Collignon <xpierro@gmail.com>
2015-09-20 01:12:19 +0800

    First test of piece loading

---
+++ picto-filesystem/src/main/java/com/github/picto/filesystem/IFilesystemMetainfo.java
@@ -0,0 +1,7 @@
+package com.github.picto.filesystem;
+
+/**
+ * Created by Pierre on 20/09/15.
+ */
+public interface IFilesystemMetainfo {
+}
---
+++ picto-filesystem/src/main/java/com/github/picto/module/FilesystemModule.java
@@ -0,0 +1,7 @@
+package com.github.picto.module;
+
+/**
+ * Created by Pierre on 20/09/15.
+ */
+public class FilesystemModule {
+}
